f5ed1ca7d75270f9913943116e402f8cc3efbf53
Jacky Ho <Jacky.ho@achievers.com>
2017-03-17 23:15:26 -0400

    added drawing feature

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,4 +1,47 @@
 var canvas = document.getElementById('drawCanvas');
 var ctx = canvas.getContext('2d');
 
+// create a flag
+var isActive = false;
+
+// array to collect coordinates
+var plots = [];
+
+function startDraw(e) {
+    isActive = true;
+}
+
+function drawOnCanvas(plots) {
+    ctx.beginPath();
+    ctx.moveTo(plots[0].x, plots[0].y);
+
+    for (i = 1; i < plots.length; i++) {
+        ctx.lineTo(plots[i].x, plots[i].y);
+    }
+    ctx.stroke();
+}
+
+function draw(e) {
+    if (!isActive) {
+        return;
+    }
+
+    // cross-browser canvas coordinates
+    var x = e.offsetX || e.layerX - canvas.offsetLeft;
+    var y = e.offsetY || e.layerY - canvas.offsetTop;
+
+    plots.push({x: x, y: y});
+
+    drawOnCanvas(plots);
+}
+
+function endDraw(e) {
+    isActive = false;
+    plots=[];
+}
+
 ctx.lineWidth = '3';
+
+canvas.addEventListener('mousedown', startDraw, false);
+canvas.addEventListener('mousemove', draw, false);
+canvas.addEventListener('mouseup', endDraw, false);
