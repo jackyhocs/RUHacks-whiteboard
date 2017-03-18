ac29455c57793b7e8078c85621776cf877f7c9b8
Erick Chin <erickchin2@gmail.com>
2017-03-18 14:04:27 -0400

    New Day New Us

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -2,7 +2,7 @@ var canvas = document.getElementById('drawCanvas');
 var canvasButton = document.getElementById('clearCanvas');
 var changeBrush = document.getElementById('changeBrush');
 var ctx = canvas.getContext('2d');
-
+var currentColour = "#000000";
 // create a flag
 var isActive = false;
 
@@ -43,45 +43,73 @@ function endDraw(e) {
 }
 
 function clearCanvas(e) {
-    ctx.fillRect(0,0,800,600);
+    // Store the current transformation matrix
+ctx.save();
+
+// Use the identity matrix while clearing the canvas
+ctx.setTransform(1, 0, 0, 1, 0, 0);
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+// Restore the transform
+ctx.restore();
 }
 
 function changeColor() {
     console.log(ctx.strokeStyle);
-    if(ctx.strokeStyle == "#ffffff") {
+    if(ctx.strokeStyle != currentColour) {
         console.log("erase");
         document.getElementById('changeBrush').textContent = "Erase";
-        ctx.strokeStyle = "#000000";
+        ctx.strokeStyle = currentColour;
     } else {
         console.log("draw");
         document.getElementById('changeBrush').textContent = "Draw";
-        ctx.strokeStyle = "#ffffff";
+        ctx.strokeStyle = "#FFFFFF";
     }
 }
 
-function initialColour(colour) {
+function changeColour(colour) {
     console.log(colour);
-    switch(typeof colour) {
-        case "red":
-            ctx.strokeStyle = "#ff0000";
+    switch(colour) {
+        case 'red':
+            ctx.strokeStyle = '#ff0000';
+            currentColour = '#ff0000';
             break;
-        case "orange":
-            ctx.strokeStyle = "#ff7600";
+        case 'orange':
+            ctx.strokeStyle = '#ff7600';
+            currentColour = '#ff7600';
             break;
-        case "yellow":
-            ctx.strokeStyle = "#fffB00";
+        case 'yellow':
+            ctx.strokeStyle = '#fffB00';
+            currentColour = '#fffB00';
             break;
-        case "cyan":
-            ctx.strokeStyle = "#00ffeb";
+        case 'cyan':
+            ctx.strokeStyle = '#00ffeb';
+            currentColour = '#00ffeb';
             break;
-        case "blue":
-            ctx.strokeStyle = "#0800ff";
+        case 'blue':
+            ctx.strokeStyle = '#0800ff';
+            currentColour = '#0800ff';
+            break;
+    }
+    document.getElementById('changeBrush').textContent = "Erase";
+}
+
+function changeSize(size) {
+    console.log(size);
+    switch(size) {
+        case 'small':
+            ctx.lineWidth = '3';
+            break;
+        case 'medium':
+            ctx.lineWidth = '6';
+            break;
+        case 'large':
+            ctx.lineWidth = '10';
             break;
     }
 }
 
 ctx.lineWidth = '3';
-ctx.fillStyle = "#fffB00";
 ctx.strokeStyle = "#000000";
 
 canvas.addEventListener('mousedown', startDraw, false);
